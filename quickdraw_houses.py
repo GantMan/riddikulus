9946b91d5188d3facac041b25a395e8e8b901981
Gant Laborde <gantman@gmail.com>
2020-12-21 13:25:13 -0600

    Create quickdraw_houses.py

---
+++ quickdraw_houses.py
@@ -0,0 +1,99 @@
+class_names = ['bird', 'owl', 'parrot', 'snake', 'snail', 'lion', 'tiger', 'mouse', 'raccoon', 'squirrel', 'skull']
+
+import urllib.request
+import os 
+import numpy as np
+from sklearn.manifold import TSNE
+# We'll use matplotlib for graphics.
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
+import matplotlib
+
+# Random state.
+RS = 20150101
+
+# We import seaborn to make nice plots.
+import seaborn as sns
+sns.set_style('darkgrid')
+sns.set_palette('muted')
+sns.set_context("notebook", font_scale=1.5,
+                rc={"lines.linewidth": 2.5})
+
+
+def download_and_load(test_split = 0.2, max_items_per_class = 10000):
+  root = 'data'
+  os.mkdir('data')
+  print('downloading ...')
+  base = 'https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/'
+  for c in class_names:
+    path = base+c+'.npy'
+    print(path)
+    urllib.request.urlretrieve(path, f'{root}/{c}.npy')
+  print('loading ...')
+  
+  #initialize variables 
+  x = np.empty([0, 784])
+  y = np.empty([0])
+
+  #load each data file 
+  for idx, file in enumerate(class_names):
+      data = np.load(f'{root}/{file}.npy')
+      data = data[0: max_items_per_class, :]
+      labels = np.full(data.shape[0], idx)
+
+      x = np.concatenate((x, data), axis=0)
+      y = np.append(y, labels)
+
+  data = None
+  labels = None
+
+  #randomize the dataset 
+  permutation = np.random.permutation(y.shape[0])
+  x = x[permutation, :]
+  y = y[permutation]
+
+  #reshape and inverse the colors 
+  x = 255 - np.reshape(x, (x.shape[0], 28, 28))
+
+  #separate into training and testing 
+  test_size  = int(x.shape[0]/100*(test_split*100))
+
+  x_test = x[0:test_size, :]
+  y_test = y[0:test_size]
+
+  x_train = x[test_size:x.shape[0], :]
+  y_train = y[test_size:y.shape[0]]
+  
+  print('Training Data : ', x_train.shape[0])
+  print('Testing  Data : ', x_test.shape[0])
+  return x_train, y_train, x_test, y_test, class_names
+
+def scatter(x, colors):
+    # We choose a color palette with seaborn.
+    palette = np.array(sns.color_palette("hls", 10))
+
+    # We create a scatter plot.
+    f = plt.figure(figsize=(8, 8))
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(x[:,0], x[:,1], lw=0, s=40,
+                    c=palette[colors.astype(np.int)])
+    plt.xlim(-25, 25)
+    plt.ylim(-25, 25)
+    ax.axis('off')
+    ax.axis('tight')
+
+    # We add the labels for each digit.
+    txts = []
+    for i in range(10):
+        # Position of each label.
+        xtext, ytext = np.median(x[colors == i, :], axis=0)
+        txt = ax.text(xtext, ytext, class_names[i], fontsize=15)
+        txt.set_path_effects([
+            PathEffects.Stroke(linewidth=5, foreground="w"),
+            PathEffects.Normal()])
+        txts.append(txt)
+        
+def plot_tsne(X, y):
+  print('calculating tsne ...')
+  proj = TSNE(random_state=RS).fit_transform(X)
+  scatter(proj, y)
